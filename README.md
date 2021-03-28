b53219ec70dd31ff9f3ae924eae3227c78658444
James <jamesbasanmhs@gmail.com>
2021-03-27 17:27:29 -0700

    Updated frontend and completed error checking

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Image-Preview-Tool
+This website allows a user to preview an image in either of two ways:
+1. inputting a URL into the search box
+2. dragging and dropping an image from the computer
+It can simulate a part of a process that requires uploading an image and
+checking to see how it will look when uploaded.
+
 ## To run
 Double-click `index.html` to display it in your browser
+
+## Example
+![Homepage image](img/home.png)
+![Preview image](img/preview.png)
--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 function imgSearch(event){
     // image searched by link
-    console.log('clicked');
     const link = document.getElementById('imgLink').value;
     previewHelper(link, true);
 }
 
 function dropHandler(event){
     // image dragged and dropped from computer
-    console.log('dropped');
     event.preventDefault();
     
     if (event.dataTransfer.items){
-        // only preview one image
-        if (event.dataTransfer.items[0].kind === 'file'){
+        const fileType = event.dataTransfer.items[0].type;
+        const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+        if (validImageTypes.includes(fileType)){
+            // preview one image
             var file = event.dataTransfer.items[0].getAsFile();
+            let fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onloadend = () => {
+                previewHelper(fileReader.result, false);
+            }
         }
         else{
-            var file = event.dataTransfer.files[0];
-        }
-        let fileReader = new FileReader();
-        try{
-        fileReader.readAsDataURL(file);
-        }
-        catch (error){
-            console.log(error);
-        }
-        fileReader.onloadend = () => {
-            previewHelper(fileReader.result, false);
+            // invalid file type
+            alert('Please drop a valid image (gif/jpeg/png)');
+            return;
         }
     }
 }
 
 function previewHelper(file, isFromLink){
     // helper function to create img from the file
+    if (file === "") {
+        alert("Please enter an image link");
+        return;
+    }
     const img = document.createElement('img');
     img.src = file;
     if (isFromLink){
@@ -42,7 +43,10 @@ function previewHelper(file, isFromLink){
         img.alt = 'image dragged from computer';
     }
     const previewer = document.getElementById('previewer');
-    previewer.removeChild(previewer.children[0]);
+    if (previewer.children[0] != null){
+        // remove the text inside the preview box
+        previewer.removeChild(previewer.children[0]);
+    }
     previewer.appendChild(img);
 }
 
